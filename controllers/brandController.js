13195baf9544228af300033af92d5fa38efbf297
bryanparmelee <bryan.parmelee@gmail.com>
2023-04-03 14:34:28 -0400

    Remove html entities from brand URLs

--- controllers/brandController.js
+++ controllers/brandController.js
@@ -3,6 +3,8 @@ const Item = require("../models/item");
 
 const async = require("async");
 
+const { body, validationResult } = require("express-validator");
+
 exports.brand_list = (req, res, next) => {
   Brand.find()
     .sort([["name", "ascending"]])
@@ -47,12 +49,49 @@ exports.brand_detail = (req, res, next) => {
 };
 
 exports.brand_create_get = (req, res, next) => {
-  res.send("Brand create-get, not implemented");
+  res.render("brand_form", { title: "Create Brand" });
 };
 
-exports.brand_create_post = (req, res, next) => {
-  res.send("Brande create-post");
-};
+exports.brand_create_post = [
+  body("brand_name", "Brand name required")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("brand_url", "Website required").trim().isURL(),
+  (req, res, next) => {
+    const errors = validationResult(req);
+
+    const brand = new Brand({
+      name: req.body.brand_name,
+      website: req.body.brand_url,
+    });
+
+    if (!errors.isEmpty()) {
+      res.render("brand_form", {
+        title: "Create Brand",
+        brand: brand,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      Brand.findOne({ name: req.body.brand_name }).exec((err, found_brand) => {
+        if (err) {
+          return next(err);
+        }
+        if (found_brand) {
+          res.redirect(found_brand.url);
+        } else {
+          brand.save((err) => {
+            if (err) {
+              return next(err);
+            }
+            res.redirect(brand.url);
+          });
+        }
+      });
+    }
+  },
+];
 
 exports.brand_delete_get = (req, res, next) => {
   res.send("Brand delete-get");
